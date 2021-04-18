e0d93848c878c083a72e660f7a9765b47792ae74
Arpit Gupta <arpit.8.a@gmail.com>
2021-04-18 13:27:47 +0530

    localStorage

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ import { makeStyles } from "@material-ui/core/styles";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
 import ButtonAppBar from "./Navbar";
+import free from "./assets/free.svg";
 
 import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    height: "100vh",
+    height: "100%",
     // margin: 0,
-    // padding: 0,
+    padding: 0,
   },
   heading: {
     paddingTop: theme.spacing(4),
@@ -30,14 +31,14 @@ const useStyles = makeStyles((theme) => ({
   card: {
     display: "flex",
     justifyContent: "space-between",
-    margin: theme.spacing(1),
+    margin: theme.spacing(1, 3),
     padding: theme.spacing(1, 2),
   },
   todo: {
     marginTop: theme.spacing(1),
   },
   list: {
-    marginTop: theme.spacing(2),
+    paddingTop: theme.spacing(2),
   },
   fab: {
     position: "fixed",
@@ -58,6 +59,25 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "row",
     paddingTop: theme.spacing(4),
+    padding: theme.spacing(1.5),
+  },
+  img: {
+    [theme.breakpoints.up("md")]: {
+      padding: theme.spacing("20%", 2),
+      paddingBottom: theme.spacing(2),
+    },
+    padding: theme.spacing("50%", 2),
+    paddingBottom: theme.spacing(2),
+  },
+  imgText: {
+    [theme.breakpoints.up("md")]: {
+      fontSize: "24px",
+    },
+  },
+  deleteBtn: {
+    fontWeight: "bold",
+    width: theme.spacing(16),
+    margin: theme.spacing(1, 3),
   },
 }));
 
@@ -67,6 +87,17 @@ const App = () => {
   const [list, setList] = useState([]);
   const [formVis, setFormVis] = useState(false);
 
+  useEffect(() => {
+    const data = localStorage.getItem("list");
+    if (data) {
+      setList(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
     <>
       <CssBaseline />
@@ -75,6 +106,7 @@ const App = () => {
         {/* <Typography variant="h4" className={classes.heading}>
           Your tasks
         </Typography> */}
+
         <Fade big>
           <div className={formVis ? classes.show : classes.hide}>
             <form className={classes.form} noValidate autoComplete="off">
@@ -91,6 +123,7 @@ const App = () => {
                 color="primary"
                 onClick={(e) => {
                   setList([...list, todo]);
+
                   setFormVis(formVis ? false : true);
                   setTodo("");
                 }}
@@ -101,6 +134,24 @@ const App = () => {
           </div>
         </Fade>
 
+        {list.length === 0 ? (
+          <div>
+            <Container maxWidth="sm" className={classes.imgContainer}>
+              <img src={free} width="100%" className={classes.img} />
+              <Typography
+                align="center"
+                variant="subtitle1"
+                className={classes.imgText}
+              >
+                Yay! You have no new tasks. Click the add button to add a new
+                task.
+              </Typography>
+            </Container>
+          </div>
+        ) : (
+          ""
+        )}
+
         <div>
           <Grid
             container
@@ -110,6 +161,21 @@ const App = () => {
             spacing={1}
             className={classes.list}
           >
+            {list.length !== 0 ? (
+              <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  className={classes.deleteBtn}
+                  onClick={() => setList([])}
+                >
+                  Remove all
+                </Button>
+              </div>
+            ) : (
+              ""
+            )}
+
             {list.map((el, key) => (
               <Fade bottom>
                 <Grid item key={key}>
@@ -120,9 +186,9 @@ const App = () => {
                     <div>
                       <IconButton
                         aria-label="delete"
-                        onClick={(e) =>
-                          setList(list.filter((element) => element !== el))
-                        }
+                        onClick={(e) => {
+                          setList(list.filter((element) => element !== el));
+                        }}
                       >
                         <DeleteIcon color="error" />
                       </IconButton>
